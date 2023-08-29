f22326a6b2c5f4f2a734b1683eb9b64ffc7ea1cc
Maximiliano Alvarez <max.alvarezr@gmail.com>
2023-08-28 20:17:42 -0600

    codigo para sample
    extraer una muestra aleatoria de la base original

---
+++ sample.R
@@ -0,0 +1,21 @@
+# Query para generar muestra aleatoria de la base
+
+# conexión con la bd
+ch <- odbcConnect("Teradata", uid = "jvelezve", pwd = "jindec2023")
+
+# nombre de la tabla
+tabla <- "PRO_SK_DATA_V.V_GT23_F341_SOL1"
+
+# tamaño de la muestra
+limite <- 1000000L
+
+# query para seleccionar observaciones aleatorias
+consulta <- paste0("SELECT *",
+                   " FROM ", tabla,
+                   " ORDER BY RANDOM()",
+                   " LIMIT ", limite)
+
+consulta
+
+# muestra
+muestra <- sqlQuery(ch, consulta)

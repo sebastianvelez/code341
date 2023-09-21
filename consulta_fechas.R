cee6c0a4b0c56ea26224c89eee4dfc74a1dc5e9d
Maximiliano Alvarez <max.alvarezr@gmail.com>
2023-09-20 18:01:46 -0600

    fechas v100
    ajuste a las queries de fechas para filtar las fechas de originacion incorrectas

--- consulta_fechas.R
+++ consulta_fechas.R
@@ -4,32 +4,46 @@ ch <- odbcConnect("Teradata", uid = "jvelezve", pwd = "jindec2023")
 # nombre de la tabla
 tabla <- "PRO_SK_DATA_V.V_GT23_F341_SOL1"
 
-# (1) Consulta usando funciones propias de Teradata (JSVV)
+# (1) Consulta usando funciones propias de Teradata (JSVV) (OK)
 consulta <- paste0("SELECT DISTINCT",
                    " CAST(FECHA_INFORMACION - 19000000 AS DATE),",
-                   " CAST(CAST(CAST(FECHA_INFORMACION AS INT) AS VARCHAR) AS DATE)",
+                   " CAST(CAST(CAST(FECHA_INFORMACION AS INTEGER) AS VARCHAR (10)) AS DATE FORMAT 'YYYYMMDD')",
                    "FROM ", tabla)
 
-df_fechas <- sqlQuery(conn, consulta)
+df_fechas <- sqlQuery(ch, consulta)
 
-# (2) Validar que el álgebra funcione como debe
+# (2.1) Validar que el álgebra funcione como debe
 consulta <- paste0(
   "SELECT FECHA_INFORMACION, FECHA_INICIAL_DEL_CREDITO,",
-  " CAST(CAST(CAST(FECHA_INFORMACION AS INT) AS VARCHAR) AS DATE) - CAST(CAST(CAST(FECHA_INICIAL_DEL_CREDITO AS INT) AS VARCHAR) AS DATE) AS DIAS_ORIGINACION",
+  " CAST(FECHA_INFORMACION - 19000000 AS DATE) - CAST(FECHA_INICIAL_DEL_CREDITO - 19000000 AS DATE) AS DIAS_ORIGINACION,",
   " FROM ", tabla,
+  " WHERE CAST(FECHA_INICIAL_DEL_CREDITO AS INTEGER) BETWEEN 19600101 AND 20231231",
   " SAMPLE 1000"
 )
 
 # Muestra de diferencia de fechas
-muestra_fechas <- sqlQuery(ch, consulta)
+system.time({muestra_fechas_1 <- sqlQuery(ch, consulta)})
+
+# (2.2) Validar que el álgebra funcione como debe
+consulta <- paste0(
+  "SELECT FECHA_INFORMACION, FECHA_INICIAL_DEL_CREDITO,",
+  " CAST(CAST(CAST(FECHA_INFORMACION AS INTEGER) AS VARCHAR (10)) AS DATE FORMAT 'YYYYMMDD') - CAST(CAST(CAST(FECHA_INICIAL_DEL_CREDITO AS INTEGER) AS VARCHAR (10)) AS DATE FORMAT 'YYYYMMDD') AS DIAS_ORIGINACION,",
+  " FROM ", tabla,
+  " WHERE CAST(FECHA_INICIAL_DEL_CREDITO AS INTEGER) BETWEEN 19600101 AND 20231231",
+  " SAMPLE 1000"
+)
+
+# Muestra de diferencia de fechas
+system.time({muestra_fechas_2 <- sqlQuery(ch, consulta)})
 
 # (3) ¿Cuántos créditos son nuevos?
 consulta <- paste0(
   "SELECT FECHA_INFORMACION,",
-  " CAST(CAST(CAST(FECHA_INFORMACION AS INT) AS VARCHAR) AS DATE) - CAST(CAST(CAST(FECHA_INICIAL_DEL_CREDITO AS INT) AS VARCHAR) AS DATE) < 90 THEN 'NUEVO' ELSE 'RECURRENTE' END AS TIPO_ACREDITADO,",
+  " CASE WHEN CAST(FECHA_INFORMACION - 19000000 AS DATE) - CAST(FECHA_INICIAL_DEL_CREDITO - 19000000 AS DATE) AS DIAS_ORIGINACION < 90 THEN 'NUEVO' ELSE 'RECURRENTE' END,",
   " COUNT(*) AS N_REG",
   " FROM ", tabla,
-  " GROUP BY FECHA_INFORMACION, TIPO_ACREDITADO"
+  " WHERE CAST(FECHA_INICIAL_DEL_CREDITO AS INTEGER) BETWEEN 19600101 AND 20231231",
+  " GROUP BY FECHA_INFORMACION, (CASE WHEN CAST(FECHA_INFORMACION - 19000000 AS DATE) - CAST(FECHA_INICIAL_DEL_CREDITO - 19000000 AS DATE) AS DIAS_ORIGINACION < 90 THEN 'NUEVO' ELSE 'RECURRENTE' END)"
 )
 
 numero_creditos <- sqlQuery(ch, consulta)
